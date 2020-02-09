228e215c1a675d18bc304302cedd82f2e22ec3f1
nfnu <42682820+nfnu@users.noreply.github.com>
2020-02-09 14:25:36 -0800

    Fire Game v1.0

---
+++ Fire Game/fire-game/UnitySDK/Assets/PenguinGame/Scripts/Fish.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fish : MonoBehaviour
+{
+    public float fishSpeed;
+
+    private float randomizedSpeed = 0f;
+    private float nextActionTime = -1f; //time (seconds) to next fish
+    private Vector3 targetPosition;
+
+    private void FixedUpdate()
+    {
+        if (fishSpeed > 0f)
+        {
+            Swim();
+        }
+    }
+
+    private void Swim()
+    {
+        if(Time.fixedTime >= nextActionTime)
+        {
+            //randomize speed
+            randomizedSpeed = fishSpeed * UnityEngine.Random.Range(0.5f, 1.5f);
+
+            //pick a random target
+            targetPosition = PenguinArea.ChooseRandomPosition(transform.parent.position, 100f, 260f, 2f, 13f);
+
+            //rotate toward the target
+            transform.rotation = Quaternion.LookRotation(targetPosition - transform.position, Vector3.up);
+
+            //calculate time to get there
+            float timeToGetThere = Vector3.Distance(transform.position, targetPosition) / randomizedSpeed;
+            nextActionTime = Time.fixedTime + timeToGetThere;
+        }
+        else
+        {
+            //make sure that the fish doesn't swim past the target
+            Vector3 moveVector = randomizedSpeed * transform.forward * Time.fixedDeltaTime;
+            if(moveVector.magnitude <= Vector3.Distance(transform.position, targetPosition))
+            {
+                transform.position += moveVector;
+            }
+            else
+            {
+                transform.position = targetPosition;
+                nextActionTime = Time.fixedTime;
+            }
+        }
+    }
+}
