704a8dd2cf5e63070a39a04e0ed8e267ca14d101
nfnu <42682820+nfnu@users.noreply.github.com>
2020-04-05 16:24:52 -0700

    Updated

---
+++ Fire Game/fire-game/UnitySDK/Assets/FireGame/Scripts/PlayerAcademy.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MLAgents;
+
+public class PlayerAcademy : Academy
+{
+    private PlayerArea[] playerAreas;
+    public override void AcademyReset()
+    {
+        //Get the penguin Areas
+        if (playerAreas == null)
+        {
+            playerAreas = FindObjectsOfType<PlayerArea>();
+        }
+        
+        //Set up areas
+        foreach (PlayerArea playerArea in playerAreas)
+        {
+            playerArea.level = resetParameters["level"];
+            playerArea.ResetArea();
+        }
+        
+    }
+}
