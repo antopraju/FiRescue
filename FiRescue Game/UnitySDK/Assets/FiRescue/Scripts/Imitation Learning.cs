29f2b94772148caba6658bf2cc292827ed823424
nfnu <42682820+nfnu@users.noreply.github.com>
2020-04-21 22:22:37 -0700

    Final Code - Training and Testing Environment

---
+++ FiRescue Game/UnitySDK/Assets/FiRescue/Scripts/Imitation Learning.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImitationLearning : MonoBehaviour
+{
+    /*
+    DogLearning:
+    use_curiosity: true
+    summary_freq: 5000
+    time_horizon: 128
+    batch_size: 128
+    buffer_size: 2048
+    hidden_units: 256
+    beta: 1.0e-2
+    max_steps: 1.3e6
+    behavioral_cloning: 
+        demo_path: C:\Users\nisha\Desktop\USC\Sem4\ml-agents-\FiRescue Game\UnitySDK\Assets\Demonstrations\tryTrainDOg_0.demo
+        strength: 0.5
+        steps: 100000
+    */
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ dog-agent-game/UnitySDK/Assets/FiRescue/Scripts/Animal.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class Animal : MonoBehaviour
+{
+    //public float health = 100f;
+    public Slider animalHealth;
+    private float lastUpdate = 0.0f;
+    RaycastHit hit_first;
+
+    private void Start()
+    {
+        //health = 100f;
+        animalHealth.value = 1;
+        lastUpdate = System.DateTime.Now.Second;
+        if (Physics.Raycast(transform.position, -Vector3.up, out hit_first) && hit_first.transform.tag == "fire")
+        {
+            lastUpdate = System.DateTime.Now.Second;
+            //health -= (1 / (hit_first.distance));
+            animalHealth.value -= (hit_first.distance * 0.1f);
+            //Debug.Log(lastUpdate + " - " + animalHealth.value);
+        }
+    }
+
+    void Update()
+    {
+        RaycastHit hit;
+        //Debug.Log(System.DateTime.Now.Second + " - last - " + lastUpdate);
+        if ((System.DateTime.Now.Second - lastUpdate > 1) && Physics.Raycast(transform.position, -Vector3.up, out hit) && hit.transform.tag == "fire")
+        {
+            //health -= (1/(hit.distance));
+            lastUpdate = System.DateTime.Now.Second;
+            animalHealth.value -= (hit.distance * 0.5f);
+            //Debug.Log(animalHealth.value);
+        }
+    }
+}
